a955b13693ee74642f8cd588e37f5c22e6f8ffa2
Cnily03 <cnily03@outlook.com>
2025-04-02 09:50:56 +0800

    :pencil: fix typo in index page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,7 @@ app.get('/', async c => {
     return c.json({
         name: c.env['SERVICE_NAME']!,
         version: c.env['VERSION']!,
-        repo: 'https://github.com/Cnily03/cf-workers-mirrors',
+        repo: 'https://github.com/Cnily03/cf-worker-mirrors',
         author: 'Cnily03',
     }, 200)
 })
